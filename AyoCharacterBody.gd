37c424f3a181f7cc235b8ae271a22a2b3ed37c01
Kenneth C. Wayman <kcwayman@scsiweb.com>
2024-03-07 09:30:12 -0500

    Revert to standard eight-way movement

--- AyoCharacterBody.gd
+++ AyoCharacterBody.gd
@@ -21,25 +21,9 @@ func determine_player_facing() -> int:
 	return facing
 
 func get_input():
-	var forward: Vector2 = get_local_mouse_position().normalized()
-	var left: Vector2 = forward.orthogonal()
-	var backward: Vector2 = left.orthogonal()
-	var right: Vector2 = backward.orthogonal()
 	var mvmt_y: float = Input.get_axis("up", "down")
 	var mvmt_x: float = Input.get_axis("left", "right")
-
-	var direction: Vector2 = Vector2()
-	if mvmt_y > 0: #DOWN
-		direction = backward
-	elif mvmt_y < 0: #UP
-		direction = forward
-
-	if mvmt_x > 0: #RIGHT
-		direction += right
-	elif mvmt_x < 0: #LEFT
-		direction += left
-
-	velocity = direction * SPEED
+	velocity = Vector2(mvmt_x, mvmt_y) * SPEED
 
 func _physics_process(delta):
 	var new_facing: int = determine_player_facing()
