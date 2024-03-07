1fe676b92035495f7478eb1ca8abd08937ee4ccb
Kenneth C. Wayman <kcwayman@scsiweb.com>
2024-03-07 10:02:38 -0500

    Movement speed depends on movement direction

--- AyoCharacterBody.gd
+++ AyoCharacterBody.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
-@export var SPEED = 150.0
+@export var FORWARD_SPEED: float = 150.0
+@export var BACKWARD_SPEED: float = 75.0
 signal facing_changed(new_facing: int)
+const EPSILON = 0.0001
 
 var player_facing: int = PlayerFacing.FACING_UP
+var facing_vectors = {
+	PlayerFacing.FACING_RIGHT: Vector2(1, 0),
+	PlayerFacing.FACING_UP: Vector2(0, 1),
+	PlayerFacing.FACING_LEFT: Vector2(-1, 0),
+	PlayerFacing.FACING_DOWN: Vector2(0, -1)
+}
 
-func determine_player_facing() -> int:
+func determine_player_facing(mouse_pos: Vector2) -> int:
 	var facing: int
-	var facing_angle: float = get_local_mouse_position().angle()
+	var facing_angle: float = mouse_pos.angle()
 
 	if facing_angle < PI / 4 and facing_angle > - PI / 4:
 		facing = PlayerFacing.FACING_RIGHT
@@ -20,15 +28,25 @@ func determine_player_facing() -> int:
 
 	return facing
 
-func get_input():
+func determine_velocity(facing: int) -> Vector2:
 	var mvmt_y: float = Input.get_axis("up", "down")
 	var mvmt_x: float = Input.get_axis("left", "right")
-	velocity = Vector2(mvmt_x, mvmt_y) * SPEED
+	var mvmt_direction: Vector2 = Vector2(mvmt_x, mvmt_y)
+	var facing_direction: Vector2 = facing_vectors[facing]
+
+	if abs(facing_direction.angle_to(mvmt_direction)) > PI / 2 + EPSILON:
+		return mvmt_direction * BACKWARD_SPEED
+	
+	return mvmt_direction * FORWARD_SPEED
 
 func _physics_process(delta):
-	var new_facing: int = determine_player_facing()
+	var mouse_pos: Vector2 = get_local_mouse_position()
+
+	var new_facing: int = determine_player_facing(mouse_pos)
 	if new_facing != player_facing:
 		facing_changed.emit(new_facing)
 		player_facing = new_facing
-	get_input()
+
+	velocity = determine_velocity(player_facing)
+
 	move_and_slide()
