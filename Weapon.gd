0806fbedb25b1b57f5cf4d9dc65fca3edc3ee8c5
Kenneth C. Wayman <kcwayman@scsiweb.com>
2024-03-23 12:35:26 -0400

    WIP: basic attack

--- Weapon.gd
+++ Weapon.gd
@@ -1,6 +1,18 @@
 extends CharacterBody2D
 
 @export var DIST: int = 30
+var is_attacking: bool = false
+var attack_timer: SceneTreeTimer
+
+func _draw():
+	if is_attacking:
+		draw_circle(position, 10, "#ffffff")
+
+func _input(event):
+	if event is InputEventMouseButton:
+		is_attacking = true
+		attack_timer = get_tree().create_timer(1.0)
+		queue_redraw()
 
 func _physics_process(delta):
 	position = get_parent().get_local_mouse_position().normalized() * DIST
